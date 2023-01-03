d818aeebd0eb8e21b4ae58509954454d370b4ae7
Jessy Pouliot <jessypouliot98@gmail.com>
2023-01-03 14:52:20 -0500

    Prep npm

---
+++ src/components/Input/index.ts
@@ -0,0 +1,5 @@
+export * from './Input';
+export * from './InputText/InputText';
+export * from './InputNumber/InputNumber';
+export * from './InputTextArea/InputTextArea';
+export * from './InputSelect/InputSelect';
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './components';
+export { Input as default } from './components';
