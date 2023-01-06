3ea5416b4ddc2c3004e70fced5cdfea27c3440f2
Jessy Pouliot <jessypouliot98@gmail.com>
2023-01-05 23:28:00 -0500

    add InputText search type

--- src/components/Input/InputText/InputText.tsx
+++ src/components/Input/InputText/InputText.tsx
@@ -2,7 +2,7 @@ import React, {forwardRef} from "react";
 import {CustomInputComponentProps, PlaceholderInputPropKeys, StringInputPropKeys} from "../types";
 import {getCommonInputProps} from "../../../utils/props";
 
-export type TextType = 'text' | 'password' | 'email' | 'url';
+export type TextType = 'text' | 'password' | 'email' | 'url' | 'search';
 type Value = string;
 export type InputStringProps = CustomInputComponentProps<
   TextType,
